9f079c7697f3da521ae522d8509bab7d17f12f92
kokilavaniV <vani59007@gmail.com>
2021-07-24 14:11:57 +0530

    portfolio
    portfolio

---
+++ Django/if.py
@@ -0,0 +1,5 @@
+year=2022
+if year>=2000 and year<=2100:
+    print("welcome to 21st century")
+else:
+    print("You are before or after the 21st century")
---
+++ Django/var.py
@@ -0,0 +1,4 @@
+name="Julie"
+age=14
+
+print("Hi my name is {} and am {}years old".format(name,age))
