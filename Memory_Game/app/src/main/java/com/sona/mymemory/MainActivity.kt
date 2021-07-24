8dc42d52dd66311da01dc443de3daa0d857bee07
kokilavaniV <vani59007@gmail.com>
2021-07-24 14:02:34 +0530

    memorygame
    MemoryGame

---
+++ Memory_Game/app/src/main/java/com/sona/mymemory/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.sona.mymemory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import com.sona.mymemory.MemoryBoardAdapter.Companion.TAG
+import com.sona.mymemory.models.BoardSize
+import com.sona.mymemory.models.MemoryCard
+import com.sona.mymemory.models.MemoryGame
+import com.sona.mymemory.utils.DEFAULT_ICONS
+
+class MainActivity : AppCompatActivity() {
+    companion object{
+
+        private const val TAG="MainActivity"
+    }
+
+    private lateinit var adapter: MemoryBoardAdapter
+    private lateinit var memoryGame: MemoryGame
+    private lateinit var rvBoard: RecyclerView
+    private lateinit var clRoot:ConstraintLayout
+
+    private lateinit var tvNumMoves:TextView
+    private lateinit var tvNumPairs:TextView
+
+    private var boardSize: BoardSize=BoardSize.EASY
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        clRoot=findViewById(R.id.clRoot)
+
+
+        rvBoard=findViewById(R.id.rvBoard)
+        tvNumMoves=findViewById(R.id.tvNumMoves)
+        tvNumPairs=findViewById(R.id.tvNumPairs)
+         memoryGame = MemoryGame(boardSize)
+
+        adapter = MemoryBoardAdapter(this,boardSize,memoryGame.cards,object : MemoryBoardAdapter.CardClickListener{
+            override fun onCardClicked(position: Int) {
+              updateGameWithFlip(position)
+            }
+
+
+        })
+        rvBoard.adapter= adapter
+        rvBoard.setHasFixedSize(true)
+        rvBoard.layoutManager=GridLayoutManager(this,boardSize.getWidth())
+
+
+}
+
+    private fun updateGameWithFlip(position: Int) {
+        if(memoryGame.haveWonGame()){
+            Snackbar.make(clRoot,"You already won!!",Snackbar.LENGTH_LONG).show()
+            return 
+        }
+        if(memoryGame.isCardFaceUp(position)){
+            Snackbar.make(clRoot,"Invalid move",Snackbar.LENGTH_SHORT).show()
+
+            return
+        }
+       if( memoryGame.flipCard(position)){
+           Log.i(TAG,"Found a match Num pairs found:${memoryGame.numPairsFound}")
+           tvNumPairs.text="Pairs:${memoryGame.numPairsFound}/${boardSize.getNumPairs()}"
+           if(memoryGame.haveWonGame()){
+               Snackbar.make(clRoot,"YOU WON! Congratulations",Snackbar.LENGTH_LONG).show()
+           }
+       }
+        tvNumMoves.text="Moves: ${memoryGame.getNumMoves()}"
+        adapter.notifyDataSetChanged()
+
+    }
+}
---
+++ Memory_Game/app/src/main/java/com/sona/mymemory/utils/Constants.kt
@@ -0,0 +1,22 @@
+package com.sona.mymemory.utils
+
+import com.sona.mymemory.R
+
+val DEFAULT_ICONS = listOf(
+        R.drawable.ic_ball,
+        R.drawable.ic_bike,
+        R.drawable.ic_cloud,
+
+        R.drawable.ic_baseline_star_24,
+        R.drawable.ic_face,
+        R.drawable.ic_fire,
+        R.drawable.ic_flower,
+        R.drawable.ic_glb,
+        R.drawable.ic_home,
+        R.drawable.ic_lamp,
+        R.drawable.ic_leaf,
+        R.drawable.ic_sun
+
+
+
+)
